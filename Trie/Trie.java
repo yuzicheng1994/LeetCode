d1c1d3c60e54b4b8ffe97ab6e3f0d2e9c3a0118d
yuzicheng1994 <157955309@qq.com>
2018-12-26 21:55:19 +0800

    commit file

---
+++ Trie/Trie.java
@@ -0,0 +1,69 @@
+import java.util.TreeMap;
+
+//https://leetcode-cn.com/problems/implement-trie-prefix-tree/
+public class Trie {
+    private class Node {
+        public boolean isWord;
+        public TreeMap<Character, Node> next;
+
+        public Node(boolean isWord) {
+            this.isWord = isWord;
+            next = new TreeMap<>();
+        }
+
+        public Node() {
+            this(false);
+        }
+    }
+
+    private Node root;
+
+    /**
+     * Initialize your data structure here.
+     */
+    public Trie() {
+        root = new Node();
+    }
+
+    /**
+     * Inserts a word into the trie.
+     */
+    public void insert(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                cur.next.put(c, new Node());
+            cur = cur.next.get(c);
+        }
+        cur.isWord = true;
+    }
+
+    /**
+     * Returns if the word is in the trie.
+     */
+    public boolean search(String word) {
+        Node cur = root;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return cur.isWord;
+    }
+
+    /**
+     * Returns if there is any word in the trie that starts with the given prefix.
+     */
+    public boolean startsWith(String prefix) {
+        Node cur = root;
+        for (int i = 0; i < prefix.length(); i++) {
+            char c = prefix.charAt(i);
+            if (cur.next.get(c) == null)
+                return false;
+            cur = cur.next.get(c);
+        }
+        return true;
+    }
+}
