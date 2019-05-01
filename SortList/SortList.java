607e257d8529d20e20238e42f5cf64bd748e2e72
yuzicheng1994 <157955309@qq.com>
2019-05-01 23:58:29 +0800

    commit file
    LeetCdoe 难度：中等 排序链表

---
+++ SortList/SortList.java
@@ -0,0 +1,46 @@
+//https://leetcode-cn.com/problems/sort-list/
+public class SortList {
+    private ListNode merge(ListNode l, ListNode r) {
+        ListNode dummyHead = new ListNode(-1);
+        ListNode cur = dummyHead;
+        while (l != null && r != null) {
+            if (l.val <= r.val) {
+                cur.next = l;
+                cur = cur.next;
+                l = l.next;
+            } else {
+                cur.next = r;
+                cur = cur.next;
+                r = r.next;
+            }
+        }
+        if (l != null) {
+            cur.next = l;
+        }
+        if (r != null) {
+            cur.next = r;
+        }
+        return dummyHead.next;
+    }
+
+    private ListNode sort(ListNode head) {
+        if (head.next == null)
+            return head;
+
+        ListNode pre = null;
+        ListNode slow = head;
+        ListNode fast = head;
+        while (fast != null && fast.next != null) {
+            pre = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        pre.next = null;
+
+        return merge(sort(head), sort(slow));
+    }
+
+    public ListNode sortList(ListNode head) {
+        return head == null ? head : sort(head);
+    }
+}
