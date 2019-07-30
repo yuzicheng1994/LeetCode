813614645e32b27ac1d03d03e28e0aec76ef4149
yuzicheng1994 <157955309@qq.com>
2019-07-30 13:07:17 +0800

    LeetCode Difficulty:Letter Combinations of a Phone Number

---
+++ LetterCombinationsOfAPhoneNumber/LetterCombinationsOfAPhoneNumber.java
@@ -0,0 +1,42 @@
+import java.util.LinkedList;
+import java.util.List;
+
+public class Solution {
+    private String letterMap[] = {
+            "abc",  //2
+            "def",  //3
+            "ghi",  //4
+            "jkl",  //5
+            "mno",  //6
+            "pqrs", //7
+            "tuv",  //8
+            "wxyz"  //9
+    };
+
+    private LinkedList<String> result;
+
+    private void findCombination(String digits, int index, String s) {
+        if (index == digits.length()) {
+            result.addLast(s);
+            return;
+        }
+
+        char c = digits.charAt(index);
+        String letters = letterMap[c - '2'];
+
+        for (int i = 0; i < letters.length(); i++)
+            findCombination(digits, index + 1, s + letters.charAt(i));
+
+        return;
+    }
+
+    public List<String> letterCombinations(String digits) {
+        result = new LinkedList<>();
+
+        if (digits.equals(""))
+            return result;
+
+        findCombination(digits, 0, "");
+        return result;
+    }
+}
