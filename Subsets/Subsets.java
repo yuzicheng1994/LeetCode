2560cdcb6ff596d5e9753858a9f473686f705286
yuzicheng1994 <157955309@qq.com>
2020-01-01 20:50:26 +0800

    LeetCode Difficulty:Medium Subsets

---
+++ Subsets/Subsets.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+//https://leetcode-cn.com/problems/subsets/
+public class Subsets {
+    private List<List<Integer>> results;
+
+    private void findSubset(List<List<Integer>> results, List<Integer> numList, int size, int startNumber, LinkedList<Integer> result) {
+        if (result.size() == size) {
+            results.add((LinkedList<Integer>) result.clone());
+            return;
+        }
+
+        for (int i = startNumber; i < numList.size(); i++) {
+            result.addLast(numList.get(i));
+            findSubset(results, numList, size, i + 1, result);
+            result.removeLast();
+        }
+    }
+
+    public List<List<Integer>> subsets(int[] nums) {
+        List<Integer> numList = new ArrayList<>();
+        for (int num : nums)
+            numList.add(num);
+
+        results = new ArrayList<>();
+        for (int i = 0; i < numList.size() + 1; i++)
+            findSubset(results, numList, i, 0, new LinkedList<>());
+
+        return results;
+    }
+}
