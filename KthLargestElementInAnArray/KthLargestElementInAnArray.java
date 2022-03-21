c8889574bbc1ebc838b4befb3fccd456e5b2ae36
yuzicheng1994 <157955309@qq.com>
2022-03-21 20:26:58 +0800

    LeetCode Difficulty:Medium Kth Largest Element in an Array

--- KthLargestElementInAnArray/KthLargestElementInAnArray.java
+++ KthLargestElementInAnArray/KthLargestElementInAnArray.java
@@ -2,16 +2,45 @@ import java.util.PriorityQueue;
 
 //https://leetcode-cn.com/problems/kth-largest-element-in-an-array/
 public class KthLargestElementInAnArray {
-    public int findKthLargest(int[] nums, int k) {
-        PriorityQueue<Integer> pq = new PriorityQueue<>(
-                (a, b) -> b - a
-        );
-        for (int num : nums)
-            pq.add(num);
+    private int findKthLargest(int[] nums, int k, int l, int r) {
+        if (l == r)
+            return nums[l];
 
-        for (int i = 1; i < k; i++) {
-            pq.remove();
+        int index = findPartition(nums, l, r);
+
+        if (index == k)
+            return nums[index];
+        else if (k < index)
+            return findKthLargest(nums, k, l, index - 1);
+        else
+            return findKthLargest(nums, k, index + 1, r);
+    }
+
+    private int findPartition(int[] nums, int l, int r) {
+        int v = nums[l];
+        int j = l;
+        int i = l + 1;
+
+        while (i <= r) {
+            if (nums[i] > v) {
+                swap(nums, i, j + 1);
+                j++;
+            }
+            i++;
         }
-        return pq.remove();
+
+        swap(nums, l, j);
+
+        return j;
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    public int findKthLargest(int[] nums, int k) {
+        return findKthLargest(nums, k - 1, 0, nums.length - 1);
     }
 }
